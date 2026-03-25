a391e9e4b38826fa765d8eb34ec4b27db2f88fde
Abraham <57534337+cyrmanj@users.noreply.github.com>
2026-03-25 17:03:53 -0600

    Add files via upload

---
+++ SQL Basics Repeat and Reverse.sql
@@ -0,0 +1,9 @@
+-- Kata: SQL Basics Repeat and Reverse
+-- Difficulty: 7 kyu
+-- Concepts: Repeat, Reverse
+-- Platform: Codewars
+
+select 
+repeat(name,3) as name,
+reverse(characteristics) as characteristics
+from monsters
