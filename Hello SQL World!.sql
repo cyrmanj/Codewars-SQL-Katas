3b9a1cdd65bed22b387a17677e7fb8421b18774d
Abraham <57534337+cyrmanj@users.noreply.github.com>
2026-05-20 19:13:03 -0600

    Update SQL syntax and add select statement

--- Hello SQL World!.sql
+++ Hello SQL World!.sql
@@ -4,11 +4,10 @@
 -- Concepts: Data Definition Language, Schema Definition
 -- Platform: Codewars
 
--- write your select statement to return hello world
 create table hello( 
-Greeting Varchar(255));
+"Greeting" Varchar(255));
 
-insert into hello(Greeting)
+insert into hello("Greeting")
 values('hello world!');
 
 select * from hello
