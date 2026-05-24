7ab772a568b76db630600d6efeda7fe448d9a03e
Abraham <57534337+cyrmanj@users.noreply.github.com>
2026-05-24 07:01:34 -0600

    Add files via upload

---
+++ Sum of odd numbers.sql
@@ -0,0 +1,8 @@
+-- Kata: Sum of odd numbers
+-- Difficulty: 7 kyu
+-- Concepts: Mathematical Pattern Recognition, Algorithmic Efficiency & Time Complexity
+-- Platform: Codewars
+
+select
+power(n,3)::int as res
+from nums
