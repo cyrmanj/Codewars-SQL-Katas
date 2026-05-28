46a91e20d1510348db56355861f72504970154e3
Abraham <57534337+cyrmanj@users.noreply.github.com>
2026-05-27 18:34:01 -0600

    Add files via upload

---
+++ String repeat.sql
@@ -0,0 +1,10 @@
+-- Kata: Splitting and Listing Unique Characters from a String Column 
+-- Difficulty: 8 kyu
+-- Concepts: Specialized functions
+-- Platform: Codewars
+
+select 
+s,
+n,
+repeat(s,n) res
+from repeatstr
