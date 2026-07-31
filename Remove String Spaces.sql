deb1ead028beb62ddba45c02cc8eb975290557b6
Abraham <57534337+cyrmanj@users.noreply.github.com>
2026-07-31 15:46:39 -0600

    Add files via upload

---
+++ Remove String Spaces.sql
@@ -0,0 +1,9 @@
+-- Kata: Remove String Spaces
+-- Difficulty: 8 kyu
+-- Concepts: String Manipulation, Pattern Matching
+-- Platform: Codewars
+
+select
+x,
+replace(x, ' ', '') as res
+from nospace
